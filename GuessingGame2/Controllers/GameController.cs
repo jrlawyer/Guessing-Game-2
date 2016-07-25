fcf64ae1f280ff10d07e37c5c24812e809bf1336
Jennifer <jrlawyer@gmail.com>
2016-07-25 09:00:47 -0400

    Initial commit.

---
+++ GuessingGame2/Controllers/GameController.cs
@@ -0,0 +1,82 @@
+﻿using GuessingGame2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuessingGame2.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            Session["Answer"] = new Random().Next(GameModel.RangeMin, GameModel.RangeMax);
+            return View();
+        } //reference GameModel.RangeMin and GameModel.RangeMax
+
+        /*private bool GuessWasCorrect(int guess)
+        {
+            return guess == (int)Session["Answer"];
+        }
+        */
+
+        private int GuessWasCorrect(int guess)
+        {
+            return guess.CompareTo((int)Session["Answer"]);
+        }
+        
+        
+                   
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index(GameModel model)
+        {
+            ViewBag.Win = GuessWasCorrect(model.Guess);
+            return View(model);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//GameController
+
+
+////get
+//public ActionResult Index()  //request from user, creation of "Answer".
+//{
+//    Session["Answer"] = new Random(1, 10);
+//    return View();
+
+//}
+
+//private bool GuessWasCorrect(int guess) //creating true/false, tieing guess and answer together.
+//{
+//    return guess == (int)Session["Answer"];
+//}
+
+//[HttpPost] //storing value in viewbag for index/view, creating a "correct" state for game.
+//public ActionResult Index(GameModel.model)
+//{
+//    ViewBag.Win = GuessWasCorrect{model.Guess}
+//    return View(Model);
+//}
+
+
+
+
+
+
---
+++ GuessingGame2/Models/GameModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame2.Models
+{
+    public class GameModel
+    {
+        [Display(Name = "Enter Your Name")]
+        [Required(ErrorMessage = "Please enter your name.")]
+        [MinLength(2, ErrorMessage = "Name is too short.")]
+        [MaxLength(20, ErrorMessage = "Name is too long")]
+        public string PlayerName
+        { get; set; }
+
+        [Display(Name = "What's Your Guess")]
+        [Required(ErrorMessage = "Please take a guess.")]
+        [Range(RangeMin, RangeMax, ErrorMessage = "Please pick a number between 1 and 10." )]
+        public int Guess
+        { get; set; }
+
+        /*create constants for rangemin and rangemax.
+       */
+        public const int RangeMin = 1;
+        public const int RangeMax = 10;
+        
+        public override string ToString()
+        {
+            return $"{PlayerName}, ({Guess})";
+        }
+    }
+}
+
+
+//public class GameModel
+//[Display(Name = "Please enter your name.")]
+//public string PlayerName
+//{ get; set; }
+
+//[Display(Name = "Please enter a guess.")]
+//public string Guess
+//{ get; set; }
+
+//public override string ToString()
+//{
+//    return $"{PlayerName}, ({Guess})";
+//}
+
